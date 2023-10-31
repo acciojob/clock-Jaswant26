2e0e7a276c74217dc69cd709a9e55b42de5ad7c6
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-31 19:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ function updateTimer() {
+      const timerElement = document.getElementById('timer');
+      const currentDate = new Date();
+      const formattedDate = currentDate.toLocaleString(); // Get current date and time in a formatted string
+      timerElement.textContent = formattedDate; // Update the content of the 'timer' element
+    }
+
+    // Call updateTimer initially to show the time immediately
+    updateTimer();
+
+    // Set an interval to update the timer every second (1000 milliseconds)
+    setInterval(updateTimer, 1000);
